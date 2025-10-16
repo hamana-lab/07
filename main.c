74c6b7bfbfca7f46a0a182855f5dd9cfa538507a
hamana-lab <hamana@sookmyung.ac.kr>
2025-10-16 15:27:04 +0900

    week07_2

--- main.c
+++ main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(void)
 {
-  int flag = 1;
-  int y;
-  while(flag != 0)
+ int i;
+ int temp = 1;
+ for (i=0;i<5;i++)
   {
-   y =3;
-   flag =0;
+   printf("temp = %d\n", temp);
+   temp++;
   }
-  y = 4;
-  
-  system("PAUSE");	
-  return 0;
+ system("PAUSE");	
+ return 0;
 }
