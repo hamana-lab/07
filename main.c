fd68f071a6e6b05288061fc4247bdca35f92be12
hamana-lab <hamana@sookmyung.ac.kr>
2025-10-16 15:36:31 +0900

    week07_3

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int inc(int counter);
 
 int main(void)
 {
- int i;
- int temp = 1;
- for (i=0;i<5;i++)
-  {
-   printf("temp = %d\n", temp);
-   temp++;
-  }
+ int i=10;
+ printf("before inc i = %d\n", i); 
+ i = inc(i);
+ printf("after inc i = %d\n", i);
+  
  system("PAUSE");	
  return 0;
+ 
+}
+
+int inc(int counter)
+{
+    counter++;
+    return counter;
 }
